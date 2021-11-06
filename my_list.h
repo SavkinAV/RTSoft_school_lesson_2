64964702140c03eae81b2a0932a784d89e7a6647
SavkinAV <93738366+SavkinAV@users.noreply.github.com>
2021-11-06 14:02:26 +0300

    Update my_list.h
    m_Node -> m_node
    вынес из класса my_list

--- my_list.h
+++ my_list.h
@@ -15,19 +15,19 @@ public:
 };
 
 template<class T>
-class m_Node {
+class m_node {
 public:
 	T data;
-	std::shared_ptr<m_Node<T>> next = nullptr;
-	std::weak_ptr  <m_Node<T>> p = nullptr;
+	std::shared_ptr<m_node<T>> next = nullptr;
+	std::weak_ptr  <m_node<T>> p = nullptr;
 
-	m_Node(T data = T()) {}
-	m_Node(T data, std::shared_ptr <m_Node<T>> next, std::weak_ptr <m_Node<T>> p) {
+	m_node(T data = T()) {}
+	m_node(T data, std::shared_ptr <m_node<T>> next, std::weak_ptr <m_node<T>> p) {
 		this->data = data;
 		this->next = next;
 		this->p = p;
 	}
-	//m_Node()
+	//m_node()
 
 };
 
@@ -37,8 +37,8 @@ class my_list{
 private:
 	
 	size_t my_lst_size = 0;
-	std::shared_ptr< m_Node<T>> head = nullptr;
-	std::shared_ptr< m_Node<T>> tale = nullptr;
+	std::shared_ptr< m_node<T>> head = nullptr;
+	std::shared_ptr< m_node<T>> tale = nullptr;
 
 	std::unique_ptr<copy_list_algrthm> copyList = nullptr;
  
@@ -47,7 +47,7 @@ public:
 	
 	}
 	my_list(T data) {
-		head = std::shared_ptr< m_Node<T>>(data);
+		head = std::shared_ptr< m_node<T>>(data);
 		tale = head;
 		my_lst_size++;
 	}
@@ -57,12 +57,12 @@ public:
 		copyList = copy_fnctn;
 	}
 
-	std::shared_ptr<m_Node<T>> get_head() {
+	std::shared_ptr<m_node<T>> get_head() {
 
 		return head;
 	}
 
-	//std::shared_ptr<m_Node<T>>get_node() {
+	//std::shared_ptr<m_node<T>>get_node() {
 	//	return 
 	//
 	//}
@@ -72,7 +72,7 @@ public:
 		if (head == nullptr)
 			return nullptr;
 		else {
-			std::shared_ptr< m_Node<T>> current = head;
+			std::shared_ptr< m_node<T>> current = head;
 			while (current <= tale) {
 				head = current->next;
 				current.reset();
@@ -82,26 +82,26 @@ public:
 		}
 	}
 	void push_front(T data) {
-		std::shared_ptr<m_Node<T>> head_tmp   = head;
-		head = std::make_shared<m_Node<T>>(data);
+		std::shared_ptr<m_node<T>> head_tmp   = head;
+		head = std::make_shared<m_node<T>>(data);
 		head->next = head_tmp;
 		my_lst_size++;	
 	}
 	void push_back(T data) {
-		std::shared_ptr<m_Node<T>> tale_tmp = tale;
-		tale = std::make_shared<m_Node<T>>(data);
+		std::shared_ptr<m_node<T>> tale_tmp = tale;
+		tale = std::make_shared<m_node<T>>(data);
 		tale_tmp->next = tale;
 		my_lst_size++;
 	}
 	void pop_front() {
-		std::shared_ptr<m_Node<T>> head_tmp = head;
+		std::shared_ptr<m_node<T>> head_tmp = head;
 		head = head_tmp->next;
 		head_tmp.reset();
 		my_lst_size--;
 	};
 	void pop_back() {
-		std::shared_ptr<m_Node<T>> tale_tmp = tale;
-		std::shared_ptr< m_Node<T>> current = head;
+		std::shared_ptr<m_node<T>> tale_tmp = tale;
+		std::shared_ptr< m_node<T>> current = head;
 		while (current < tale) {
 			current = current->next;
 		}
@@ -121,7 +121,7 @@ public:
 		if (list->head == nullptr)
 			return nullptr;
 		std::unique_ptr<my_list> new_list;
-		std::shared_ptr<m_Node<T>> current = list->get_head();
+		std::shared_ptr<m_node<T>> current = list->get_head();
 		while (current->next != nullptr) {
 			new_list->push_back(current);
 			current = current->next;
